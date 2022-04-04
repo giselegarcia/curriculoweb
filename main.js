004f439b63a1a8fb52315abfe14706581f7214e0
giselegarcia <80002777+giselegarcia@users.noreply.github.com>
2022-04-04 15:51:22 -0300

    files

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('.conteudo-principal-escrito-titulo');
+document.querySelector('.conteudo-principal-escrito-titulo').textContent
+document.querySelector('.conteudo-principal-escrito-titulo').textContent = "Currículo de Gisele Garcia"
+var titulo = document.querySelector(".conteudo-principal-escrito-titulo");
+
+
